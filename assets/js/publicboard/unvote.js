45b89708ceb26b046ba1093a0c4d1ae537381efe
Wanderson Camargo <wandersonwhcr@gmail.com>
2022-08-05 18:22:04 -0300

    Unvote cards

--- assets/js/publicboard/unvote.js
+++ assets/js/publicboard/unvote.js
@@ -1,3 +1,25 @@
 (function () {
-  console.log('Hello, World');
+
+  var icon = document.createElement("i");
+  icon.setAttribute("class", "ri-eraser-line fill");
+
+  var text = document.createTextNode("Unvote All");
+
+  var button = document.createElement('button');
+  button.setAttribute("class", "secondary-icon import-btn share-button");
+  button.append(icon);
+  button.append(text);
+
+  button.onclick = function (event) {
+    Array.from(document.getElementsByClassName('unvote-link'))
+      .forEach(function (element) {
+        element.click();
+      });
+  };
+
+  window.onload = function () {
+    var controls = document.getElementsByClassName("easy-menu-controls")[0];
+    controls.append(button);
+  };
+
 })()
